435dd784b5c0555e5d08fc3e7199f91a7d099ce3
Ethan N. Ha <ethannicha@gmail.com>
2023-03-12 01:44:51 -0800

    Implement custom trapezoid prism, stack, b4 plane

--- assignment2/a2/Code.java
+++ assignment2/a2/Code.java
@@ -1,8 +1,6 @@
 package a2;
 
 import java.nio.*;
-import java.util.Vector;
-
 import javax.swing.*;
 import java.lang.Math;
 import static com.jogamp.opengl.GL4.*;
@@ -11,32 +9,30 @@ import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.GLContext;
 import com.jogamp.opengl.util.*;
 import com.jogamp.common.nio.Buffers;
-
 import org.joml.*;
 
 public class Code extends JFrame implements GLEventListener
-{	private GLCanvas myCanvas;
+{	
+	private GLCanvas myCanvas;
 	private int renderingProgram;
 	private int vao[] = new int[1];
 	private int vbo[] = new int[2];
+	private double startTime = 0.0;
+	private double elapsedTime;
+	private double tf;
 	private float cameraX, cameraY, cameraZ;
-	private float cubeLocX, cubeLocY, cubeLocZ;
 
 	// allocate variables for display() function
 	private FloatBuffer vals = Buffers.newDirectFloatBuffer(16);  // buffer for transfering matrix to uniform
+	private Matrix4fStack mvStack = new Matrix4fStack(5);	//model-view matrix stack
 	private Matrix4f pMat = new Matrix4f();  // perspective matrix
-	private Matrix4f vMat = new Matrix4f();  // view matrix
-	private Matrix4f mMat = new Matrix4f();  // model matrix
-	private Matrix4f mvMat = new Matrix4f(); // model-view matrix
-	private Matrix4f laMat = new Matrix4f(); // look-at matrix
-
-	private Vector3f fwdV = new Vector3f();		// eye-target
-	private Vector3f sideV = new Vector3f();	// -fwd * Y vector
-	private Vector3f upV = new Vector3f();		// side * -fwd
-	private Vector3f yV = new Vector3f(); 	// Y vector
-
 	private int mvLoc, pLoc;
 	private float aspect;
+
+	// object variables
+	private int shuttleTexture;
+	private int numObjVertices;
+	private ImportedModel myModel;
 
 	public Code()
 	{	setTitle("CSC 155 - Assignment 2");
@@ -45,11 +41,16 @@ public class Code extends JFrame implements GLEventListener
 		myCanvas.addGLEventListener(this);
 		this.add(myCanvas);
 		this.setVisible(true);
+		Animator animator = new Animator(myCanvas);
+		animator.start();
 	}
 
 	public void display(GLAutoDrawable drawable)
 	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		gl.glClear(GL_COLOR_BUFFER_BIT);
 		gl.glClear(GL_DEPTH_BUFFER_BIT);
+		elapsedTime = System.currentTimeMillis() - startTime;
+		tf = elapsedTime/1000.0;
 
 		gl.glUseProgram(renderingProgram);
 
@@ -57,47 +58,74 @@ public class Code extends JFrame implements GLEventListener
 		pLoc = gl.glGetUniformLocation(renderingProgram, "p_matrix");
 
 		aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
-		pMat.setPerspective((float) Math.toRadians(60.0f), aspect, 0.1f, 1000.0f);
-
-		vMat.translation(-cameraX, -cameraY, -cameraZ);
-
-		mMat.translation(cubeLocX, cubeLocY, cubeLocZ);
-
-		mvMat.identity();
-		mvMat.mul(vMat);
-		mvMat.mul(mMat);
-
-		yV = new Vector3f(0, 1, 0);
-		fwdV = (new Vector3f(cameraX-cubeLocX, cameraY-cubeLocY, cameraZ-cubeLocZ)).normalize();
-		sideV = (new Vector3f((fwdV).mul(yV))).normalize();
-		
-		laMat.setLookAt(sideV, upV, fwdV);
-
-		gl.glUniformMatrix4fv(mvLoc, 1, false, mvMat.get(vals));
+		pMat.identity().setPerspective((float) Math.toRadians(60.0f), aspect, 0.1f, 1000.0f);
 		gl.glUniformMatrix4fv(pLoc, 1, false, pMat.get(vals));
 
+		// push view matrix initial empty state
+		mvStack.pushMatrix();
+		mvStack.translate(-cameraX, -cameraY, -cameraZ);		//moves matrix to camera location
+
+		// parent cube object
+		mvStack.pushMatrix();			//push parent matrix
+		mvStack.translate(0.0f, 0.0f, 0.0f);
+		mvStack.pushMatrix();
+		mvStack.rotate((float)tf, (float)Math.cos(tf), (float)Math.sin(tf), 0.0f);
+		gl.glUniformMatrix4fv(mvLoc, 1, false, mvStack.get(vals));
 		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 		gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
 		gl.glEnableVertexAttribArray(0);
-
 		gl.glEnable(GL_DEPTH_TEST);
-		gl.glDepthFunc(GL_LEQUAL);
-
 		gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+		mvStack.popMatrix();
+
+		// trapezoidal prism object
+		mvStack.pushMatrix();			//push child matrix
+		mvStack.translate((float)Math.sin(tf)*4.0f, 0.0f, (float)Math.cos(tf)*4.0f);
+		mvStack.pushMatrix();
+		mvStack.rotate((float)tf, 0.0f, 1.0f, 0.0f);
+		mvStack.scale(0.6f, 0.6f, 0.6f);
+		gl.glUniformMatrix4fv(mvLoc, 1, false, mvStack.get(vals));
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+		gl.glEnableVertexAttribArray(0);
+		gl.glEnable(GL_DEPTH_TEST);
+		gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+		mvStack.popMatrix();
+
+		// mini trapezoid prism
+		mvStack.pushMatrix();
+		mvStack.translate((float)Math.cos(tf), (float)Math.sin(tf), 0);
+		mvStack.rotate((float)tf, 0.0f, 0.0f, 1.0f);
+		mvStack.scale(0.2f, 0.2f, 0.2f);
+		gl.glUniformMatrix4fv(mvLoc, 1, false, mvStack.get(vals));
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+		gl.glEnableVertexAttribArray(0);
+		gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+		mvStack.popMatrix();  
+		mvStack.popMatrix();  
+		mvStack.popMatrix();
+		mvStack.popMatrix();
 	}
 
 	public void init(GLAutoDrawable drawable)
-	{	GL4 gl = (GL4) drawable.getGL();
+	{	
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+		startTime = System.currentTimeMillis();
 		renderingProgram = Utils.createShaderProgram("a2/vertShader.glsl", "a2/fragShader.glsl");
 		setupVertices();
-		cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
-		cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+		
+		cameraX = 0.0f; cameraY = 0.0f; cameraZ = 10.0f;
 	}
 
 	private void setupVertices()
-	{	GL4 gl = (GL4) GLContext.getCurrentGL();
-		float[] vertexPositions =
-		{	-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+	{	
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+
+		// 36 vertices of the 12 triangles making up a 2 x 2 x 2 cube centered at the origin
+		float[] cubePositions =
+		{	// (x,y,z) three times each line
+			-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
 			1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
 			1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
 			1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
@@ -110,14 +138,34 @@ public class Code extends JFrame implements GLEventListener
 			-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
 			1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
 		};
+		// 36 vertices of 12 triangles as well to create a trapezoidal prism
+		float[] trapezoidPositions = 
+		{
+			-1.0f, -0.5f, -1.0f, 1.0f, -0.5f, -1.0f, 1.0f, -0.5f, 1.0f,		//bottom face
+			1.0f, -0.5f, 1.0f, -1.0f, -0.5f, 1.0f, -1.0f, -0.5f, -1.0f,	
+			-1.0f, -0.5f, -1.0f, 1.0f, -0.5f, -1.0f, -0.5f, 0.5f, -0.5f,	//front face
+			-0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 1.0f, -0.5f, -1.0f,
+			1.0f, -0.5f, -1.0f, 0.5f, 0.5f, -0.5f, 1.0f, -0.5f, 1.0f,		//right face
+			1.0f, -0.5f, 1.0f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 
+			0.5f, 0.5f, 0.5f, 1.0f, -0.5f, 1.0f, -1.0f, -0.5f, 1.0f, 		//back face
+			-1.0f, -0.5f, 1.0f, 0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 
+			-0.5f, 0.5f, 0.5f, -1.0f, -0.5f, 1.0f, -1.0f, -0.5f, -1.0f, 	//left face
+			-1.0f, -0.5f, -1.0f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, 
+			-0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 		//top face
+			0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f
+		};
 
 		gl.glGenVertexArrays(vao.length, vao, 0);
 		gl.glBindVertexArray(vao[0]);
 		gl.glGenBuffers(vbo.length, vbo, 0);
 
 		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-		FloatBuffer vertBuf = Buffers.newDirectFloatBuffer(vertexPositions);
-		gl.glBufferData(GL_ARRAY_BUFFER, vertBuf.limit()*4, vertBuf, GL_STATIC_DRAW);
+		FloatBuffer cubeBuf = Buffers.newDirectFloatBuffer(cubePositions);
+		gl.glBufferData(GL_ARRAY_BUFFER, cubeBuf.limit()*4, cubeBuf, GL_STATIC_DRAW);
+
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		FloatBuffer tpzBuf = Buffers.newDirectFloatBuffer(trapezoidPositions);
+		gl.glBufferData(GL_ARRAY_BUFFER, tpzBuf.limit()*4, tpzBuf, GL_STATIC_DRAW);
 	}
 
 	public static void main(String[] args) { new Code(); }
--- assignment2/a2/fragShader.glsl
+++ assignment2/a2/fragShader.glsl
@@ -1,11 +1,15 @@
 #version 430
 
+in vec2 tc;
 out vec4 color;
+out vec4 tex;
 
 uniform mat4 mv_matrix;
 uniform mat4 p_matrix;
+layout (binding=0) uniform sampler2D s;
 
 void main(void)
 {	
-    color = vec4(1.0, 0.0, 0.0, 1.0);
+    color = vec4(0.0, 1.0, 0.0, 1.0);
+    tex = texture(s,tc);
 }
