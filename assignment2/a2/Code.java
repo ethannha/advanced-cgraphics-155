337477ad1986f20cc2ab390760bbb71e9851a9b0
Ethan N. Ha <ethannicha@gmail.com>
2023-03-11 16:35:55 -0800

    Add a2 starting files

---
+++ assignment2/a2/Code.java
@@ -0,0 +1,126 @@
+package a2;
+
+import java.nio.*;
+import java.util.Vector;
+
+import javax.swing.*;
+import java.lang.Math;
+import static com.jogamp.opengl.GL4.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.util.*;
+import com.jogamp.common.nio.Buffers;
+
+import org.joml.*;
+
+public class Code extends JFrame implements GLEventListener
+{	private GLCanvas myCanvas;
+	private int renderingProgram;
+	private int vao[] = new int[1];
+	private int vbo[] = new int[2];
+	private float cameraX, cameraY, cameraZ;
+	private float cubeLocX, cubeLocY, cubeLocZ;
+
+	// allocate variables for display() function
+	private FloatBuffer vals = Buffers.newDirectFloatBuffer(16);  // buffer for transfering matrix to uniform
+	private Matrix4f pMat = new Matrix4f();  // perspective matrix
+	private Matrix4f vMat = new Matrix4f();  // view matrix
+	private Matrix4f mMat = new Matrix4f();  // model matrix
+	private Matrix4f mvMat = new Matrix4f(); // model-view matrix
+	private Matrix4f laMat = new Matrix4f(); // look-at matrix
+
+	private Vector3f fwdV = new Vector3f();		// eye-target
+	private Vector3f sideV = new Vector3f();	// -fwd * Y vector
+	private Vector3f upV = new Vector3f();		// side * -fwd
+	private Vector3f yV = new Vector3f(); 	// Y vector
+
+	private int mvLoc, pLoc;
+	private float aspect;
+
+	public Code()
+	{	setTitle("CSC 155 - Assignment 2");
+		setSize(600, 600);
+		myCanvas = new GLCanvas();
+		myCanvas.addGLEventListener(this);
+		this.add(myCanvas);
+		this.setVisible(true);
+	}
+
+	public void display(GLAutoDrawable drawable)
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		gl.glClear(GL_DEPTH_BUFFER_BIT);
+
+		gl.glUseProgram(renderingProgram);
+
+		mvLoc = gl.glGetUniformLocation(renderingProgram, "mv_matrix");
+		pLoc = gl.glGetUniformLocation(renderingProgram, "p_matrix");
+
+		aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
+		pMat.setPerspective((float) Math.toRadians(60.0f), aspect, 0.1f, 1000.0f);
+
+		vMat.translation(-cameraX, -cameraY, -cameraZ);
+
+		mMat.translation(cubeLocX, cubeLocY, cubeLocZ);
+
+		mvMat.identity();
+		mvMat.mul(vMat);
+		mvMat.mul(mMat);
+
+		yV = new Vector3f(0, 1, 0);
+		fwdV = (new Vector3f(cameraX-cubeLocX, cameraY-cubeLocY, cameraZ-cubeLocZ)).normalize();
+		sideV = (new Vector3f((fwdV).mul(yV))).normalize();
+		
+		laMat.setLookAt(sideV, upV, fwdV);
+
+		gl.glUniformMatrix4fv(mvLoc, 1, false, mvMat.get(vals));
+		gl.glUniformMatrix4fv(pLoc, 1, false, pMat.get(vals));
+
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+		gl.glEnableVertexAttribArray(0);
+
+		gl.glEnable(GL_DEPTH_TEST);
+		gl.glDepthFunc(GL_LEQUAL);
+
+		gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+
+	public void init(GLAutoDrawable drawable)
+	{	GL4 gl = (GL4) drawable.getGL();
+		renderingProgram = Utils.createShaderProgram("a2/vertShader.glsl", "a2/fragShader.glsl");
+		setupVertices();
+		cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+		cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+	}
+
+	private void setupVertices()
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		float[] vertexPositions =
+		{	-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+			-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+		};
+
+		gl.glGenVertexArrays(vao.length, vao, 0);
+		gl.glBindVertexArray(vao[0]);
+		gl.glGenBuffers(vbo.length, vbo, 0);
+
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		FloatBuffer vertBuf = Buffers.newDirectFloatBuffer(vertexPositions);
+		gl.glBufferData(GL_ARRAY_BUFFER, vertBuf.limit()*4, vertBuf, GL_STATIC_DRAW);
+	}
+
+	public static void main(String[] args) { new Code(); }
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
+	public void dispose(GLAutoDrawable drawable) {}
+}
